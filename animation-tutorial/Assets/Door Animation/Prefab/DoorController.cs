56d9419065ebb380f0825546d1c23965fa45dc93
Xavier Fox <xfactor_fox12@yahoo.com>
2017-03-02 13:21:25 -0500

    Added an environment and a FPS controller

---
+++ animation-tutorial/Assets/Door Animation/Prefab/DoorController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+
+	public Animator animator;
+
+	public void OnTriggerEnter(Collider collider)
+	{
+		OpenDoor ();
+	}
+
+	public void OnTriggerExit(Collider collider)
+	{
+		CloseDoor ();
+	}
+
+	public void OpenDoor()
+	{
+		animator.SetInteger ("Door Open", 1);
+		animator.SetInteger ("Door Close", 0);
+	}
+
+	public void CloseDoor()
+	{
+		animator.SetInteger ("Door Open", 0);
+		animator.SetInteger ("Door Close", 1);
+	}
+}
