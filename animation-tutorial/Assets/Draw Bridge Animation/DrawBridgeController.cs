c2274a44d7f960aca21934e5681441f25ba6f182
Xavier Fox <xfactor_fox12@yahoo.com>
2017-03-16 11:48:16 -0400

    Created a scene, that deals with a draw bridge

---
+++ animation-tutorial/Assets/Draw Bridge Animation/DrawBridgeController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawBridgeController : MonoBehaviour {
+
+	public Animator animator;
+
+	public void OnTriggerEnter(Collider collider)
+	{
+		OpenDoor ();
+	}
+
+	public void OnTriggerExit(Collider collider)
+	{
+		CloseDoor ();
+	}
+
+	public void OpenDoor()
+	{
+		animator.SetBool ("IsOpen", true);
+		animator.SetBool ("IsClosed", false);
+	}
+
+	public void CloseDoor()
+	{
+		animator.SetBool ("IsOpen", false);
+		animator.SetBool ("IsClosed", true);
+	}
+}
